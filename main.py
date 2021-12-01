0dbac2fade30c19645f628cfe1666e8814855daa
Your Name <you@example.com>
2021-12-01 12:50:15 +0700

    new vn text image

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import text_to_speech
 import img_to_text
 
-image = 'Write-a-Paragraph-Step-10-Version-4.jpg'
+image = 'Image148.jpg'
 
 text = img_to_text.change_to_text(image)
 temp = text_to_speech.change_to_speech(text)
